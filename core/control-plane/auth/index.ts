b5a50be0529dbc90c4c36276bfbe0e782cbeecd3
Agung Nugroho <agung.nugroho@beamtrail.com>
2025-03-04 12:34:27 +0400

    Testing login

--- core/control-plane/auth/index.ts
+++ core/control-plane/auth/index.ts
@@ -7,14 +7,14 @@ export async function getAuthUrlForTokenPage(
   useOnboarding: boolean,
 ): Promise<string> {
   const env = await getControlPlaneEnv(ideSettingsPromise);
-  const url = new URL("https://api.workos.com/user_management/authorize");
+  const url = new URL("https://chatai.directory.internal/auth");
   const params = {
     response_type: "code",
     client_id: env.WORKOS_CLIENT_ID,
-    redirect_uri: `${env.APP_URL}tokens/${useOnboarding ? "onboarding-" : ""}callback`,
+    redirect_uri: `${env.APP_URL}/oauth/oidc/callback`,
     // redirect_uri: "http://localhost:3000/tokens/callback",
     state: uuidv4(),
-    provider: "authkit",
+    provider: "Authentik",
   };
   Object.keys(params).forEach((key) =>
     url.searchParams.append(key, params[key as keyof typeof params]),
