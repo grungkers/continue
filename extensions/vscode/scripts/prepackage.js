960dddc76658a794a00a958ab7eeb31e0472c0f2
grungkers <indonesi@me.com>
2025-03-07 15:37:33 +0400

    Update prepackage.js

--- extensions/vscode/scripts/prepackage.js
+++ extensions/vscode/scripts/prepackage.js
@@ -49,7 +49,7 @@ console.log("[info] Using target: ", target);
 
 const exe = os === "win32" ? ".exe" : "";
 
-const isInGitHubAction = false;
+const isInGitHubAction = true;
 
 const isArmTarget =
   target === "darwin-arm64" ||
